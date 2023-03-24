19ba50ac070d55843b11b2fb60d7d8fa02ffd9ee
최서윤 <cinnamon1995@naver.com>
2023-03-24 22:49:54 +0900

    Header작성(2)

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,24 +1,23 @@
 import React from 'react'
-import {RxMagnifyingGlass} from 'react-icons/rx'
-import {BsCart} from 'react-icons/bs'
-import {CgProfile} from 'react-icons/cg'
+import { RxMagnifyingGlass } from 'react-icons/rx'
+import { BsCart } from 'react-icons/bs'
+import { CgProfile } from 'react-icons/cg'
 import styled from 'styled-components'
-import Cookies from "universal-cookie";
-import { BlueButton } from './CommenButton';
-import {AiOutlineDown} from 'react-icons/ai';
+import Cookies from 'universal-cookie'
+import { BlueButton } from './CommenButton'
+import { AiOutlineDown } from 'react-icons/ai'
 
-
-export const cookies = new Cookies();
+export const cookies = new Cookies()
 
 function Header() {
   const token = cookies.get('token')
   return (
     <header>
-      {token ? (
+      {!token ? (
         <>
           {/* 토큰이 없을 때 */}
           <NavTop>
-            <div className='logo'>로고</div>
+            <div className="logo">로고</div>
             <div>
               <MarginZoreP>커뮤니티</MarginZoreP>
               <MarginZoreP>쇼핑</MarginZoreP>
@@ -35,7 +34,7 @@ function Header() {
               <MarginZoreP>회원가입</MarginZoreP>
             </div>
           </NavTop>
-          
+
           <NavBottom>
             <div>쇼핑홈</div>
             <div>카테고리</div>
@@ -45,7 +44,7 @@ function Header() {
         <>
           {/* 토큰이 존재할 때 */}
           <div>
-            <div className='logo'>로고</div>
+            <div className="logo">로고</div>
             <div>na</div>
             <div>
               <MarginZoreP>
@@ -55,12 +54,12 @@ function Header() {
                 <BsCart />
               </MarginZoreP>
               <MarginZoreP>{CgProfile}</MarginZoreP>
-              <MarginZoreP><BlueButton width="60px" height="40px" color="white" innerText="글쓰기"/></MarginZoreP>
-              
+              <MarginZoreP>
+                <BlueButton width="60px" height="40px" color="white" innerText="글쓰기" />
+              </MarginZoreP>
             </div>
           </div>
-          
-    
+
           <NavBottom>
             <div>쇼핑홈</div>
             <div>카테고리</div>
@@ -68,7 +67,7 @@ function Header() {
         </>
       )}
     </header>
-  );
+  )
 }
 
 export default Header
@@ -91,7 +90,7 @@ const NavTop = styled.nav`
 
   div:nth-child(2) {
     display: flex;
-    gap:30px;
+    gap: 30px;
   }
 
   div:nth-child(3) {
@@ -99,14 +98,10 @@ const NavTop = styled.nav`
     justify-content: flex-end;
     align-items: center;
     padding-right: 10px;
-    gap:30px;
-
+    gap: 30px;
   }
-  
-  
 `
 const NavBottom = styled.nav`
   display: flex;
   gap: 10px;
-  
 `
