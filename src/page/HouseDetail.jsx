393c87b44b7aea123b20f92d699bd85b51588657
최서윤 <cinnamon1995@naver.com>
2023-03-28 21:44:32 +0900

    house 리스트 (조회)(house.js) 및 상세조회(housedetail.js) 및 입력칸(housewrite) 만드는 중

--- src/page/HouseDetail.jsx
+++ src/page/HouseDetail.jsx
@@ -1,13 +1,65 @@
 import React from 'react'
+import axios from 'axios'
+import Footer from '../components/Footer'
+import Header from '../components/Header'
+import { useParams } from 'react-router-dom'
+import { useQuery } from 'react-query'
+import { BsSymmetryHorizontal } from 'react-icons/bs'
+import styled from 'styled-components'
+import { BsEmojiSmile } from 'react-icons/bs'
 
+const HouseDetail = () => {
+  const { id } = useParams()
+  console.log('ID -> ', id)
 
-function HouseDetail() {
-  
+  const { data } = useQuery(['GET_HOUSE', id], async () => {
+    const response = await axios.get(`http://43.201.116.11:8080/houses/${id}`)
+    console.log('data-->', data)
+    return response.data
+  })
+
+  console.log('data', data)
+
   return (
     <>
-    <div>집들이페이지</div>
+      <Header />
+      <div>
+        <img src={data?.image} alt={data?.image} />
+        <ExDiv>
+          <h3>온라인 집들이</h3>
+          <h1>{data?.title}</h1>
+          <ProfileDiv>
+            <div>
+              <BsEmojiSmile />
+            </div>
+            <div>{data?.nickname}</div>
+          </ProfileDiv>
+          <DeDiv>
+            <div>{data?.dwellingtype}</div>
+            <div>{data?.average}</div>
+            <div>{data?.region}</div>
+          </DeDiv>
+          <div>{data?.contents}</div>
+        </ExDiv>
+      </div>
+      <Footer />
     </>
   )
 }
 
 export default HouseDetail
+
+const ExDiv = styled.div`
+  margin: auto;
+  width: 60%;
+`
+const ProfileDiv = styled.div`
+  display: flex;
+  gap: 10px;
+  size: 20px;
+`
+const DeDiv = styled.div`
+  margin-top: 30px;
+  background-color: #f6f7f9;
+  display: flex;
+`
---
+++ src/utils/createQueryKey.js
@@ -0,0 +1,6 @@
+export const keys = {
+  GET_HOUSES: ['GET_HOUSES'],
+  GET_HOUSE: ['GET_HOUSE'],
+  GET_CMT: ['GET_CMT'],
+  GET_CMTS: ['GET_CMTS'],
+}
